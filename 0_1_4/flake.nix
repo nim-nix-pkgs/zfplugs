5d5689810b19faeb223a89e85cd9bd2ee25c5838
hugosenari <hugosenari@gmail.com>
2022-03-22 00:39:27 -0300

    chore: re-index ../nimpkgs/z/zfplugs

--- 0_1_4/flake.nix
+++ 0_1_4/flake.nix
@@ -9,13 +9,14 @@
   
   inputs.src-zfplugs-0_1_4.flake = false;
   inputs.src-zfplugs-0_1_4.owner = "zendbit";
-  inputs.src-zfplugs-0_1_4.ref   = "refs/tags/0.1.4";
+  inputs.src-zfplugs-0_1_4.ref   = "0_1_4";
   inputs.src-zfplugs-0_1_4.repo  = "nim.zfplugs";
   inputs.src-zfplugs-0_1_4.type  = "github";
   
   inputs."zfcore".owner = "nim-nix-pkgs";
   inputs."zfcore".ref   = "master";
   inputs."zfcore".repo  = "zfcore";
+  inputs."zfcore".dir   = "v1_1_7";
   inputs."zfcore".type  = "github";
   inputs."zfcore".inputs.nixpkgs.follows = "nixpkgs";
   inputs."zfcore".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
