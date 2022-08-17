074d08d10f8d6feebb5e9b25eb94a1585222d133
hugosenari <hugosenari@gmail.com>
2022-08-17 20:41:37 -0300

    chore: re index zfplugs

--- v0_1_14/flake.nix
+++ v0_1_14/flake.nix
@@ -7,11 +7,19 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-zfplugs-0_0_19.flake = false;
-  inputs.src-zfplugs-0_0_19.ref   = "refs/tags/0.0.19";
-  inputs.src-zfplugs-0_0_19.owner = "zendbit";
-  inputs.src-zfplugs-0_0_19.repo  = "nim.zfplugs";
-  inputs.src-zfplugs-0_0_19.type  = "github";
+  inputs.src-zfplugs-v0_1_14.flake = false;
+  inputs.src-zfplugs-v0_1_14.ref   = "refs/tags/v0.1.14";
+  inputs.src-zfplugs-v0_1_14.owner = "zendbit";
+  inputs.src-zfplugs-v0_1_14.repo  = "nim.zfplugs";
+  inputs.src-zfplugs-v0_1_14.type  = "github";
+  
+  inputs."zfdbms".owner = "nim-nix-pkgs";
+  inputs."zfdbms".ref   = "master";
+  inputs."zfdbms".repo  = "zfdbms";
+  inputs."zfdbms".dir   = "v0_0_7";
+  inputs."zfdbms".type  = "github";
+  inputs."zfdbms".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zfdbms".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   inputs."zfcore".owner = "nim-nix-pkgs";
   inputs."zfcore".ref   = "master";
@@ -21,16 +29,24 @@
   inputs."zfcore".inputs.nixpkgs.follows = "nixpkgs";
   inputs."zfcore".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
+  inputs."mustache".owner = "nim-nix-pkgs";
+  inputs."mustache".ref   = "master";
+  inputs."mustache".repo  = "mustache";
+  inputs."mustache".dir   = "v0_4_3";
+  inputs."mustache".type  = "github";
+  inputs."mustache".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mustache".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-zfplugs-0_0_19"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-zfplugs-v0_1_14"];
     over = if builtins.pathExists ./override.nix 
            then { override = import ./override.nix; }
            else { };
   in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
-    src  = deps."src-zfplugs-0_0_19";
+    src  = deps."src-zfplugs-v0_1_14";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   } );
